2dcc5b000ab85e79d551b85441eeb5a316821c45
kogecoo <kogecoo@gmail.com>
2015-02-23 02:14:40 +0900

    add "perl_path" attribute

--- attributes/default.rb
+++ attributes/default.rb
@@ -7,5 +7,6 @@ default["mecab-perl"] = {
   "support" => {
     "0.996" => { "checksum" => "4e34fab226800dd99f8d8f2dbd3d891d99059990", "checksum_type" => :SHA1 },
     "0.994" => { "checksum" => "f5a57d1c13f987e001172d870f0e1e73cf6d8610", "checksum_type" => :SHA1 }
-  }
+  },
+  "perl_path" => nil
 }
